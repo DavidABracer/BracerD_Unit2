926c7d70d460cb8eba8bb22536f5557c622d5d23
DavidABracer <davidbracer@yahoo.com>
2021-03-06 12:07:40 -0600

    Unit2 Lesson A done
    Finishing Leasson A Unit 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalImput;
+    public float speed = 10.0f;
+    private float xRange = 19.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalImput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalImput * Time.deltaTime * speed);
+
+        if( transform.position.x>xRange) 
+        {
+       transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
