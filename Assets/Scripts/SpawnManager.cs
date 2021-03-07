bb8ebb6a5368fbf50ba538353b7d26cc241dc1f5
DavidABracer <davidbracer@yahoo.com>
2021-03-07 01:28:16 -0600

    Unit 2 Lesson C completed
    Finished Unit 2 Lesons C

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalsPrefabs;
+    private float xPosRange = 13;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            float randXPos = Random.Range(-xPosRange, xPosRange);
+            int animalPrefabIndex = Random.Range(0, animalsPrefabs.Length);
+            Vector3 randPos = new Vector3(randXPos, 0, 29);
+            Instantiate(animalsPrefabs[animalPrefabIndex], randPos, 
+            animalsPrefabs[animalPrefabIndex].transform.rotation);
+        }
+    }
+}
